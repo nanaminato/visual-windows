989cee040f996b82483b900f987379685e1ed8bc
nanaminato <bethaniebell8474@gmail.com>
2025-08-24 19:01:33 +0800

    修复显示错误

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const serverEnvironment = {
     protocol: 'http',
-    baseUrl: "localhost:5111",
+    // baseUrl: "localhost:5111",
     // baseUrl: "192.168.1.7:5000",
     // baseUrl: "192.168.1.8:5000",
     // baseUrl: "192.168.1.9:5000",
-    // baseUrl: "localhost:5000",
+    baseUrl: "localhost:5000",
 }
